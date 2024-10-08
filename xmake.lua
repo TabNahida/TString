280656912fa5022d7490074e12def3d804a760de
Tab Ye <tabye320@outlook.com>
2024-10-08 16:17:53 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,14 @@
 add_languages("cxx20")
 
+target("tstring")
+    set_kind("static")
+    add_headerfiles("include/TString.hpp")
+    add_includedirs("include")
+target_end()
+
 target("Test")
     set_kind("binary")
     add_files("src/main.cpp")
-    add_headerfiles("include/TString.hpp")
+    add_deps("tstring")
     add_includedirs("include")
 target_end()
