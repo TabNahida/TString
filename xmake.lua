fbd732023a14f94cb75ac2a2030473d4abb02909
Tab Ye <tabye320@outlook.com>
2024-10-19 22:40:37 +0800

    remove unused add_xxx

--- xmake.lua
+++ xmake.lua
@@ -4,9 +4,7 @@ add_languages("cxx20")
 
 target("tstring")
     set_kind("headeronly")
-    -- add_files("src/TString.cpp")
     add_headerfiles("include/TString.hpp")
-    -- add_includedirs("include")
 target_end()
 
 target("MainTest")
